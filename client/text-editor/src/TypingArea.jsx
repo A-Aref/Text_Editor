2d66bc6536897a50d4ef9774d2480a0f86cea3bb
A-Aref <abdallahahmadaref@gmail.com>
2024-03-21 01:22:37 +0200

    modified:   client/text-editor/package-lock.json 	modified:   client/text-editor/src/TypingArea.css 	modified:   client/text-editor/src/TypingArea.jsx

--- client/text-editor/src/TypingArea.jsx
+++ client/text-editor/src/TypingArea.jsx
@@ -1,49 +1,34 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 import Navbar from "react-bootstrap/Navbar";
 
-import SaveIcon from '@mui/icons-material/Save';
+import SaveIcon from "@mui/icons-material/Save";
 import IconButton from "@mui/material/IconButton";
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import FormatItalicIcon from '@mui/icons-material/FormatItalic';
-import FormatBoldIcon from '@mui/icons-material/FormatBold';
-import "./TypingArea.css";
-import { useQuill } from 'react-quilljs';
-import 'quill/dist/quill.snow.css'; // Add css for snow theme
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
+import ReactQuill,{ Quill } from "react-quill";
+
+import "./TypingArea.css";
+import "quill/dist/quill.snow.css";
 
 function TypingArea(props) {
 
-  const modules = {
-    toolbar: [
-      ['bold', 'italic'],
-    ],
+  const params = useParams();
+  const navigate = useNavigate();
+  const ref  = useRef()
 
-  };
-  const { quill, quillRef } = useQuill({ modules, placeholder: "Start typing here" });
-  const [value, setValue] = useState("")
-  const params = useParams()
-  const navigate = useNavigate()
-  React.useEffect(() => {
-    if (quill) {
-      quill.on('text-change', () => {
+  const [value, setValue] = useState("");
+  const [value1, setValue1] = useState("");
 
-        setValue(quillRef.current.firstChild.innerHTML);
-      });
-    }
-  }, [quill]);
+  useEffect(()=>{
+    if(value)
+      console.log(ref.current.getEditor().getContents())
+  },[value])
+
   const handleBack = () => {
     // Handle the save functionality
-    navigate('/Docs')
-  };
-  const handleBold = () => {
-    // Handle the save functionality
-    console.log("Bold clicked");
-  };
-  const handleItalic = () => {
-    // Handle the save functionality
-    console.log("Italic clicked");
+    navigate("/Docs");
   };
   const handleSave = () => {
     // Handle the save functionality
@@ -52,34 +37,41 @@ function TypingArea(props) {
 
   return (
     <div style={{ justifyContent: "center" }}>
-      <Navbar bg="black" style={{ justifyContent: "space-between", }} sticky="top">
+      <Navbar
+        bg="black"
+        style={{ justifyContent: "space-between" }}
+        sticky="top"
+      >
         <IconButton style={{ color: "white" }} onClick={handleBack}>
           <ArrowBackIcon fontSize="large" />
         </IconButton>
-        {!props.edit ?
-          <div className="rounded">
-            {/* <button className="edit-button" onClick={handleBold}>
-              <FormatBoldIcon fontSize="large" />
-            </button>
-            <button className=" edit-button" onClick={handleItalic}>
-              <FormatItalicIcon fontSize="large" />
-            </button> */}
+        {!props.edit ? (
+          <div className="rounded" style={{ display: "flex" }}>
+            <div style={{ display: "flex" }} id="my-quill-toolbar">
+              <button className="edit-button ql-bold" />
+              <button className=" edit-button ql-italic" />
+            </div>
             <button className=" edit-button" onClick={handleSave}>
-              <SaveIcon className="saveIcon" fontSize="medium" />
-              <div>Save</div>
+              <SaveIcon className="saveIcon" sx={{ fontSize: 44 }} />
             </button>
           </div>
-          :
-          <div></div>
-        }
-        <h2 style={{ marginRight: "3rem" }}><i>{params.id}</i></h2>
+        ) : (
+          <div id="my-quill-toolbar"></div>
+        )}
+        <h2 style={{ marginRight: "3rem" }}>
+          <i>{params.id}</i>
+        </h2>
       </Navbar>
       <br />
-      <div style={{ justifyContent: "center", display: "flex" }}>
-        <div id="my-quill-toolbar"  >
-          <div ref={quillRef} />
-        </div>
-      </div>
+      <ReactQuill
+        ref = {ref}
+        theme="snow"
+        value={value}
+        onChange={setValue}
+        modules={{ toolbar: { container: "#my-quill-toolbar" } }}
+        readOnly={props.edit}
+        id="editor"
+      />
     </div>
   );
 }
