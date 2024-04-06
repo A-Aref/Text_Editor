55ebcf95ec6be11e4e0d781fd896036cf0785f5c
joex03 <youssefaymann22@gmail.com>
2024-04-06 20:20:28 +0200

    Update UsersRepository.java

--- server/texteditor/src/main/java/dev/texteditor/Users/UsersRepository.java
+++ server/texteditor/src/main/java/dev/texteditor/Users/UsersRepository.java
@@ -1,16 +1,14 @@
 package dev.texteditor.Users;
 
-
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
-
-
-
 @Repository
 public interface UsersRepository extends MongoRepository<Users,ObjectId> {
     
-Optional<Users> findByUserId(String userId);
+    Optional<Users> findByUserId(String userId);
+    boolean existsByEmail(String email); 
+    Users findByEmail(String email); 
 }
