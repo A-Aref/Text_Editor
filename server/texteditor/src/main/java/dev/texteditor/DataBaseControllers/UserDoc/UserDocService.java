78a072fcd9a76dc39c77e67547607331ea75ff5f
MohamedSobh032 <mohamed.sobh032@gmail.com>
2024-04-21 21:40:50 +0200

    shared update

--- server/texteditor/src/main/java/dev/texteditor/DataBaseControllers/UserDoc/UserDocService.java
+++ server/texteditor/src/main/java/dev/texteditor/DataBaseControllers/UserDoc/UserDocService.java
@@ -2,7 +2,7 @@ package dev.texteditor.DataBaseControllers.UserDoc;
 
 import java.util.List;
 
-
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -33,11 +33,27 @@ public class UserDocService {
     @Autowired
     private MongoTemplate mongoTemplate;
 
+    /* 2 --> owner , 1 --> update , 0 --> does not exists */
+    public boolean checker(String UserID, String DocID) {
+        List<UserDoc> doc = userDocRepository.findByDocId(DocID);
+        for (int i = 0; i < doc.size(); i++) {
+            String CurUser = doc.get(i).getUserId();
+            if (UserID.equals(CurUser)) { return true; }
+        }
+        return false;
+    }
+
     public void updateSharedAccess(String UserId, String DocId, String Role) {
         Criteria criteria = Criteria.where("userId").is(UserId).and("docId").is(DocId);
         Query query = new Query(criteria);
         Update update = new Update().set("role", Role);
         mongoTemplate.updateFirst(query, update, UserDoc.class);
+    }
+
+    public void createSharedAccess(String userId, String docId, String role) {
+        ObjectId generatedId = new ObjectId();
+        UserDoc userDoc = new UserDoc(generatedId, userId, docId, role);
+        mongoTemplate.insert(userDoc);
     }
 
 }
